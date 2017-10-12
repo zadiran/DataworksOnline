4da60b9dce444849733349d367b6457c53d379e2
Konstantin Zadiran <konstantin.zadiran@gmail.com>
2017-10-13 00:52:01 +0300

    Changed database connection string settings

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import uuid
 class Config():
 	CSRF_ENABLED = True
 	SECRET_KEY = str(uuid.uuid4())
-	SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost/DataworksOnline'
+	SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
